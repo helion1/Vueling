b49b79a93f0df74a4ac4e499e029abce8839760f
user <user@DESKTOP-8BB5QKE>
2018-06-27 22:41:54 +0200

    Close: #23 Unit Tests with XUnit + console DONE!

---
+++ Calculator/Calculator.Unit.Tests/CalculadoraFixture.cs
@@ -0,0 +1,25 @@
+﻿using Calculator.Contracts;
+using NMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Unit.Tests {
+    public class CalculadoraFixture {
+        private MockFactory _factory = new MockFactory();
+        public ICalculadora MockObject { get; private set; }
+
+        public CalculadoraFixture() {
+            var mock = _factory.CreateMock<ICalculadora>();
+
+            mock.Expects.One.MethodWith(x => x.Division(9, 3)).WillReturn(3);
+            mock.Expects.One.MethodWith(x => x.Multiplicacion(3, 2)).WillReturn(6);
+            mock.Expects.One.MethodWith(x => x.Resta(7, 3)).WillReturn(4);
+            mock.Expects.One.MethodWith(x => x.Suma(2, 5)).WillReturn(7);
+
+            MockObject = mock.MockObject;
+        }
+    }
+}
---
+++ Calculator/Calculator.Unit.Tests/CalculatorUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculator.Unit.Tests{
+    public class CalculatorUnitTests : IClassFixture<CalculadoraFixture>{
+        CalculadoraFixture fixture;
+
+        public CalculatorUnitTests(CalculadoraFixture fixture) {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void DivisionTest() {
+            Assert.Equal(3, fixture.MockObject.Division(9, 3));
+        }
+
+        [Fact]
+        public void MultiplicacionTest() {
+            Assert.Equal(6, fixture.MockObject.Multiplicacion(3, 2));
+        }
+
+        [Fact]
+        public void RestaTest() {
+            Assert.Equal(4, fixture.MockObject.Resta(7, 3));
+        }
+
+        [Fact]
+        public void SumaTest() {
+            Assert.Equal(7, fixture.MockObject.Suma(2, 5));
+        }
+
+
+    }
+}
