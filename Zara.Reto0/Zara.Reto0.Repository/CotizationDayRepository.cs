681e0585b8463be724c00dbf2752d450cb5263db
user <user@DESKTOP-8BB5QKE>
2018-07-09 14:09:17 +0200

    Working on ISSUE #43. List of transaction Days Complete.

--- Zara.Reto0/Zara.Reto0.Repository/CotizationDayRepository.cs
+++ Zara.Reto0/Zara.Reto0.Repository/CotizationDayRepository.cs
@@ -44,17 +44,34 @@ namespace Zara.Reto0.Utils
             return ListadoFinal;
         }
 
-        /*
+        
         public decimal Ganancia() {
-            List<CotizationDay> ListadoDiaSemanal = new List<CotizationDay>();
+            List<CotizationDay> ListadoDiasTransacciones = new List<CotizationDay>();
+            List<DateTime> ListadoFechas = new List<DateTime>();
+
+            foreach(CotizationDay cp in ListadoFinal) {
+                ListadoFechas.Add(cp.Fecha);
+            }
+
             DateTime diaSemanal = DateTime.Parse("Thursday");
-            DateTime UltimoJueves;
+            DateTime diaDeCompra;
+            decimal res = 0;
+            int mesActual = 0;
 
-            foreach (CotizationDay cp in ListadoFinal) {
-                UltimoJueves = DateTimeUtil.GetLastThursdayOfTheMonth(cp.Fecha);
+            foreach (DateTime dt in ListadoFechas) {
+                if (mesActual != dt.Month) {
+                    mesActual = dt.Month;
+                    diaDeCompra = DateTimeUtil.GetLastThursdayOfTheMonth(dt).AddDays(1);
 
-                if (cp.Fecha == UltimoJueves.AddDays(1)) {
-                    ListadoDiaSemanal.Add(cp);
+                    while (!ListadoFechas.Contains(diaDeCompra)) {
+                        diaDeCompra = diaDeCompra.AddDays(1);
+                    }
+
+                    foreach(CotizationDay cd in ListadoFinal) {
+                        if(DateTime.Compare(cd.Fecha, diaDeCompra) == 0) {
+                            ListadoDiasTransacciones.Add(cd);
+                        }
+                    }
                 }
 
                 
@@ -62,9 +79,9 @@ namespace Zara.Reto0.Utils
 
             
 
-            return ;
+            return res;
         }
-        */
+        
         
         
         public decimal SumaCierres() {
