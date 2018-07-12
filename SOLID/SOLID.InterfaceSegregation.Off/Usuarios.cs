a85c0a5ac1584b4e71e6e0394d824826cd796d84
user <user@DESKTOP-8BB5QKE>
2018-07-12 04:24:51 +0200

    Closed #50, Closed #51. Together. ISP Done

---
+++ SOLID/SOLID.InterfaceSegregation.Off/Usuarios.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.InterfaceSegregation.Off {
+    public interface IUsuario {
+        void Acceder();
+        void Moderar();
+        void Registrarse(string datos);
+    }
+
+
+    public class Usuario : IUsuario {
+        public string Nombre { get; set; }
+        public void Acceder() {
+        }
+        public void Moderar() {
+            throw new NotImplementedException();
+        }
+        public void Registrarse(string datos) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Moderador : IUsuario {
+        public string Nombre { get; set; }
+        public void Acceder() { }
+        public void Moderar() { }
+
+        public void Registrarse(string datos) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NoRegistrado : IUsuario {
+        public void Acceder() {
+            throw new NotImplementedException();
+        }
+
+        public void Moderar() {
+            throw new NotImplementedException();
+        }
+
+        public void Registrarse(string datos) { }
+    }
+
+}
+
+    
+
---
+++ SOLID/SOLID.InterfaceSegregation.On/Usuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.InterfaceSegregation.Off {
+    public interface IUsuario {
+        void Acceder();
+    }
+
+    public interface IModerador {
+        void Acceder();
+        void Moderar();
+    }
+
+    public interface INoRegistrado {
+        void Registrarse(string datos);
+    }
+
+
+    public class Usuario : IUsuario {
+        public string Nombre { get; set; }
+        public void Acceder() {
+        }
+    }
+
+    public class Moderador : IModerador, IUsuario {
+        public string Nombre { get; set; }
+        public void Acceder() { }
+        public void Moderar() { }
+
+    }
+
+    public class NoRegistrado : INoRegistrado {
+        public void Registrarse(string datos) { }
+    }
+
+}
+
+    
+
